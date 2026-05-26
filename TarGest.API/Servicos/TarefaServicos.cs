85fab173aace42707fbb5068e36d98d842ef4931
Divangamene <divangameneluyeye@gmail.com>
2026-05-26 22:10:48 +0100

    feat: Criar regra de negocio CriarTarefa

--- TarGest.API/Servicos/TarefaServicos.cs
+++ TarGest.API/Servicos/TarefaServicos.cs
@@ -1,6 +1,26 @@
-﻿namespace TarGest.API.Servicos
+﻿using TarGest.API.Conexao;
+using TarGest.API.Entidades;
+
+namespace TarGest.API.Servicos
 {
     public class TarefaServicos
     {
+        
+        public readonly ApiDbContext _servico;
+        public TarefaServicos(ApiDbContext servico)
+        {
+            _servico= servico;
+        }
+
+        //criar 
+
+        public async Task<Tarefa> CriarTarefa(Tarefa tarefa)
+        {
+            _servico.Tarefas.Add(tarefa);
+            await _servico.SaveChangesAsync();
+            return tarefa;
+
+
     }
+        }
 }
