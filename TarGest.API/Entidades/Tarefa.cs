5a90e058c1458bfcd6cb4427ad97644ce5a26069
Divangamene <divangameneluyeye@gmail.com>
2026-05-26 21:26:27 +0100

    feat: Adicionar entidade Tarefa

---
+++ TarGest.API/Entidades/Tarefa.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TarGest.API.Entidades
+{
+    public class Tarefa
+    {
+        [Key]
+        public int tarefaId { get; set; }
+        public string tarefaTitulo { get; set; } = string.Empty;
+        public string tarefaDescricao { get; set; } = string.Empty;
+        public DateTime tarefaPrazo { get; set; }
+        public DateTime horaInicio { get; set; } = DateTime.Now;
+    }
+}
