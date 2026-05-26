c73fe0a9bf9ed4781ff4c1ffc338fc06703d6350
Divangamene <divangameneluyeye@gmail.com>
2026-05-26 21:31:01 +0100

    feat: Adicionar DTO tarefa

---
+++ TarGest.API/DTOS/TarefaDto.cs
@@ -0,0 +1,13 @@
+﻿using TarGest.API.Enum;
+
+namespace TarGest.API.DTOS
+{
+    public class TarefaDto
+    {
+        public string tarefaTitulo { get; set; } = string.Empty;
+        public string tarefaDescricao { get; set; } = string.Empty;
+        public EstadoTarefa estadoTarefa { get; set; } = EstadoTarefa.pedente;
+        public DateTime prazoTarefa { get; set; }
+        public DateTime horaInicio { get; set; } = DateTime.Now;
+    }
+}
