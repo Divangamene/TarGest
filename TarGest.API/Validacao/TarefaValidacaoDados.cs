61976c1f9f07a4c24ff7865729ff894198e98d1c
Divangamene <divangameneluyeye@gmail.com>
2026-05-31 02:16:09 +0100

    feat: Adicionar FluentValidation para a validação de dados

---
+++ TarGest.API/Validacao/TarefaValidacaoDados.cs
@@ -0,0 +1,12 @@
+﻿using System.Data;
+using TarGest.API.Entidades;
+
+namespace TarGest.API.Validacao
+{
+    public class TarefaValidacaoDados
+    {
+        public TarefaValidacaoDados(Tarefa tarefa)
+        {
+        }
+    }
+}
